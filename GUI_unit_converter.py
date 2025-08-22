e87811463bec7a1ae78008d952e2bc3a8fb811d9
ratib <ratibhoque@gmail.com>
2025-08-22 19:21:13 +1000

    added dropdown menu

--- GUI_unit_converter.py
+++ GUI_unit_converter.py
@@ -4,10 +4,20 @@ root = ctk.CTk() # creates the main window
 
 root.title("Unit Converter") # sets a title of the window
 
-root.geometry("300x250") # sets the dimensions of the window
+root.geometry("500x500") # sets the dimensions of the window
 
-value_var = ctk.StringVar() # variable to hold the input value
+value_var = ctk.StringVar(master=root) # variable to hold the input value
+unit_var = ctk.StringVar(master=root, value="meters")
 
-entry = ctk.Entry(root, textvariable=value_var) # creates an entry widget
+def convert():
+    pass
+
+unit = ctk.CTkOptionMenu(
+    root, variable=unit_var, values=["CentiMeters","Meters", "Kilometres"]
+    )
+unit.pack(pady=5, padx=5)
+
+entry = ctk.CTkEntry(root, textvariable=value_var) # creates an entry widget
+entry.pack(pady=10, padx=5)
 
 root.mainloop()

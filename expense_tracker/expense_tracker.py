bb55977b8c144012c8cfc580276ca5ad000093db
ratib <ratibhoque@gmail.com>
2025-09-06 17:46:37 +1000

    more layout elements

--- expense_tracker/expense_tracker.py
+++ expense_tracker/expense_tracker.py
@@ -33,8 +33,23 @@ month.grid(row=2, column=3, sticky="ew")
 subtitle = ctk.CTkLabel(root, text="Expenses list")
 subtitle.grid(row=3, column=0)
 
-expenses_list = ctk.CTkFrame(root)
+expenses_list = ctk.CTkFrame(root, bg_color="blue")
 expenses_list.pack(row=4)
+
+
+
+expense_date = ctk.CTkLabel(expenses_list, text="Date")
+expense_date.grid(row=0, column=0)
+
+expense_category = ctk.CTkLabel(expenses_list, text="Category")
+expense_category.grid(row=0, column=1)
+
+expense_amount = ctk.CTkLabel(expenses_list, text="Amount")
+expense_amount.grid(row=0, column=2)
+
+expense_note = ctk.CTkLabel(expenses_list, text="Note")
+expense_note.grid(row=0, column=3)
+
 
 
 
